413a9428eb0a1890975b3fab568f7a7a0cce535b
EshaArif-1234 <eshaarif0322@gmail.com>
2026-04-08 17:03:14 +0500

    added the new images in customer section and removed the previous one

--- components/about/MissionVisionSection.tsx
+++ components/about/MissionVisionSection.tsx
@@ -10,235 +10,225 @@ const MissionVisionSection = () => {
   const logos: LogoCard[] = [
     {
       id: '1',
-      logo: '/Images/clients/Dan-Dan.webp',
+      logo: '/Images/Logo Final/logo1.webp',
       name: 'Client 1'
     },
     {
       id: '2',
-      logo: '/Images/clients/Avari.webp',
+      logo: '/Images/Logo Final/logo2.webp',
       name: 'Client 2'
     },
     {
       id: '3',
-      logo: '/Images/clients/Cafe-Zouk.webp',
+      logo: '/Images/Logo Final/logo3.webp',
       name: 'Client 3'
     },
     {
       id: '4',
-      logo: '/Images/clients/Aitchison-College.png.webp',
+      logo: '/Images/Logo Final/logo4.webp',
       name: 'Client 4'
     },
     {
       id: '5',
-      logo: '/Images/clients/Avari-Express.webp',
+      logo: '/Images/Logo Final/logo5.webp',
       name: 'Client 5'
     },
     {
       id: '6',
-      logo: '/Images/clients/Coffee-Bean.webp',
+      logo: '/Images/Logo Final/logo6.webp',
       name: 'Client 6'
     },
     {
       id: '7',
-      logo: '/Images/clients/Cothm.webp',
+      logo: '/Images/Logo Final/logo7.webp',
       name: 'Client 7'
     },
     {
       id: '8',
-      logo: '/Images/clients/Dagwood.webp',
+      logo: '/Images/Logo Final/logo8.webp',
       name: 'Client 8'
     },
      {
       id: '9',
-      logo: '/Images/clients/DEfense-Raya.webp',
+      logo: '/Images/Logo Final/logo9.webp',
       name: 'Client 9'
     },
     {
       id: '10',
-      logo: '/Images/clients/Deja.webp',
+      logo: '/Images/Logo Final/logo10.webp',
       name: 'Client 10'
     },
     {
       id: '11',
-      logo: '/Images/clients/Fuoco.webp',
+      logo: '/Images/Logo Final/logo11.webp',
       name: 'Client 11'
     },
     {
       id: '12',
-      logo: '/Images/clients/Ginyaki.webp',
+      logo: '/Images/Logo Final/logo12.webp',
       name: 'Client 12'
     },
     {
       id: '13',
-      logo: '/Images/clients/Gloria-Jeans.webp',
+      logo: '/Images/Logo Final/logo13.webp',
       name: 'Client 13'
     },
      {
       id: '14',
-      logo: '/Images/clients/Gloria-Jeans.webp',
+      logo: '/Images/Logo Final/logo14.webp',
       name: 'Client 14'
     }, 
     {
       id: '15',
-      logo: '/Images/clients/Gourmet-Restaurants.webp',
+      logo: '/Images/Logo Final/logo15.webp',
       name: 'Client 15'
     }, 
     {
       id: '16',
-      logo: '/Images/clients/Hardees.png.webp',
+      logo: '/Images/Logo Final/logo16.webp',
       name: 'Client 16'
     }, 
     {
       id: '17',
-      logo: '/Images/clients/HD.webp',
+      logo: '/Images/Logo Final/logo17.webp',
       name: 'Client 17'
     }, 
     {
       id: '18',
-      logo: '/Images/clients/HOtel-One.webp',
+      logo: '/Images/Logo Final/logo18.webp',
       name: 'Client 18'
     }, 
     {
       id: '19',
-      logo: '/Images/clients/Howdy.webp',
+      logo: '/Images/Logo Final/logo19.webp',
       name: 'Client 19'
     }, 
     {
       id: '20',
-      logo: '/Images/clients/Jalal-Sons-1.webp',
+      logo: '/Images/Logo Final/logo20.webp',
       name: 'Client 20'
     }, 
     {
       id: '21',
-      logo: '/Images/clients/Lahore-Gymkhana.webp',
+      logo: '/Images/Logo Final/logo21.webp',
       name: 'Client 21'
     }, 
     {
       id: '22',
-      logo: '/Images/clients/Lahore-School-of-Economics.png.webp',
+      logo: '/Images/Logo Final/logo22.webp',
       name: 'Client 22'
     }, 
     {
       id: '23',
-      logo: '/Images/clients/Layers.png.webp',
+      logo: '/Images/Logo Final/logo23.webp',
       name: 'Client 23'
     }, 
     {
       id: '24',
-      logo: '/Images/clients/Mandrin-Kitchen.png.webp',
+      logo: '/Images/Logo Final/logo24.webp',
       name: 'Client 24'
     }, 
     {
       id: '25',
-      logo: '/Images/clients/Mandrin-Kitchen.webp',
+      logo: '/Images/Logo Final/logo25.webp',
       name: 'Client 25'
     }, 
     {
       id: '26',
-      logo: '/Images/clients/Mcdonalds.webp',
+      logo: '/Images/Logo Final/logo26.webp',
       name: 'Client 26'
     }, 
     {
       id: '27',
-      logo: '/Images/clients/Mei-Kong.webp',
+      logo: '/Images/Logo Final/logo27.webp',
       name: 'Client 27'
     }, 
     {
       id: '28',
-      logo: '/Images/clients/Miso.webp',
+      logo: '/Images/Logo Final/logo28.webp',
       name: 'Client 28'
     }, 
     {
       id: '29',
-      logo: '/Images/clients/Nandos.png.webp',
+      logo: '/Images/Logo Final/logo29.webp',
       name: 'Client 29'
     }, 
     {
       id: '30',
-      logo: '/Images/clients/Nisa-Sultan.webp',
+      logo: '/Images/Logo Final/logo30.webp',
       name: 'Client 30'
     }, 
     {
       id: '31',
-      logo: '/Images/clients/PC-Hotel.webp',
+      logo: '/Images/Logo Final/logo31.webp',
       name: 'Client 31'
     }, 
     {
       id: '32',
-      logo: '/Images/clients/Pink-Pistachio.webp',
+      logo: '/Images/Logo Final/logo32.webp',
       name: 'Client 32'
     },
      {
       id: '33',
-      logo: '/Images/clients/Ramada-Hotel.webp',
+      logo: '/Images/Logo Final/logo33.webp',
       name: 'Client 33'
     }, 
     {
       id: '34',
-      logo: '/Images/clients/Rina-Kitchenette.webp',
+      logo: '/Images/Logo Final/logo34.webp',
       name: 'Client 34'
     }, 
     {
       id: '35',
-      logo: '/Images/clients/Salt-n-Paper.webp',
+      logo: '/Images/Logo Final/logo35.webp',
       name: 'Client 35'
     }, 
     {
       id: '36',
-      logo: '/Images/clients/Sarpinos.webp',
+      logo: '/Images/Logo Final/logo36.webp',
       name: 'Client 36'
     }, 
     {
       id: '37',
-      logo: '/Images/clients/Sashas.webp',
+      logo: '/Images/Logo Final/logo37.webp',
       name: 'Client 37'
     }, 
     {
       id: '38',
-      logo: '/Images/clients/Savoey-Hotel.webp',
+      logo: '/Images/Logo Final/logo38.webp',
       name: 'Client 38'
     }, 
     {
       id: '39',
-      logo: '/Images/clients/Scafa.webp',
+      logo: '/Images/Logo Final/logo39.webp',
       name: 'Client 39'
     }, 
     {
       id: '40',
-      logo: '/Images/clients/Serena-Hotels.webp',
+      logo: '/Images/Logo Final/logo40.webp',
       name: 'Client 40'
     }, 
     {
       id: '41',
-      logo: '/Images/clients/Shezan.png.webp',
+      logo: '/Images/Logo Final/logo41.webp',
       name: 'Client 41'
     }, 
     {
       id: '42',
-      logo: '/Images/clients/Steak-Away.webp',
+      logo: '/Images/Logo Final/logo42.webp',
       name: 'Client 42'
     }, 
     {
       id: '43',
-      logo: '/Images/clients/Subway.webp',
+      logo: '/Images/Logo Final/logo43.webp',
       name: 'Client 43'
     }, 
     {
       id: '44',
-      logo: '/Images/clients/Sumo.webp',
+      logo: '/Images/Logo Final/logo44.webp',
       name: 'Client 44'
     }, 
-    {
-      id: '45',
-      logo: '/Images/clients/The-carnivore.webp',
-      name: 'Client 45'
-    }, 
-    {
-      id: '46',
-      logo: '/Images/clients/The-Mad-Itlian.webp',
-      name: 'Client 46'
-    }
-
+ 
 
   ];
 
@@ -259,7 +249,7 @@ const MissionVisionSection = () => {
         {/* Section Header */}
         <div className="text-center mb-16">
           <h2 className="text-4xl md:text-5xl font-bold text-gray-900 mb-4">
-            Our <span className="text-orange-500">Partners</span>
+            Our <span className="text-orange-500">Customers</span>
           </h2>
           <p className="text-lg text-gray-600 max-w-3xl mx-auto">
             Trusted by leading businesses across industries
